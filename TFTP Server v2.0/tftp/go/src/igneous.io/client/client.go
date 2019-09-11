61d6228df3d2c0573c01f9f76e8a02625c23a624
Deb Jones <debjones513@gmail.com>
2019-09-11 12:12:13 -0700

    Client makes a read request, server sends back an ack. Build the tftp package used by the client and server

--- TFTP Server v2.0/tftp/go/src/igneous.io/client/client.go
+++ TFTP Server v2.0/tftp/go/src/igneous.io/client/client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"../tftp"
+	"fmt"
 	"log"
 	"net"
 )
@@ -14,12 +16,34 @@ func main() {
 	}
 	defer conn.Close()
 
-	//simple write
-	conn.Write([]byte("Hello from client"))
+	// Construct a read packet and send it to the server
+
+	var pr_send = new(tftp.PacketRequest)
+	pr_send.Mode = "octet"
+	pr_send.Op = 1
+	pr_send.Filename = "xyz"
+
+	b := make([]byte, 1024)
+	b = tftp.Packet.Serialize(pr_send)
+
+	conn.Write(b)
+
+	//simple write - send to server
+	//conn.Write([]byte("Hello from client"))
 
 	//simple Read
+	//buffer := make([]byte, 1024)
+	//conn.Read(buffer)
+
+	// Process a reply fronm the server
 	buffer := make([]byte, 1024)
 	conn.Read(buffer)
+
+	pr_receive, err := tftp.ParsePacket(buffer)
+
+	fmt.Printf("Parsed Packet Err: %d Packet: %+v \n", err, pr_receive)
+
 	log.Println(buffer)
+	//fmt.Printf("Reply from Server: %v \n", buffer)
 
 }
--- TFTP Server v2.0/tftp/go/src/igneous.io/tftp/cmd/tftpd/main.go
+++ TFTP Server v2.0/tftp/go/src/igneous.io/tftp/cmd/tftpd/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"../../../tftp"
 	"fmt"
 	"log"
-"net"
+	"net"
 )
 
 /// http://computernetworkingsimplified.in/application-layer/tftp-works/
+// https://tools.ietf.org/html/rfc1350
 
 func main() {
-	// listen to incoming udp packets
+
+	// Setup to listen on port 69 for all IPs on the local network (localhost only).
+
 	pc, err := net.ListenPacket("udp", ":69")
 	if err != nil {
 		log.Fatal(err)
@@ -17,15 +21,23 @@ func main() {
 	defer pc.Close()
 
 	fmt.Printf("Connection: %+v \n", pc)
+	fmt.Printf("Local Addr: %+v \n", pc.LocalAddr())
+
+	// Handle requests
 
 	for {
+
 		buf := make([]byte, 1024)
 		n, addr, err := pc.ReadFrom(buf)
 		if err != nil {
 			continue
 		}
 
-		fmt.Printf("Count: %d Addr: %s Err: %d Buffer: %s \n", n, addr, err, buf)
+		fmt.Printf("ReadFrom Count: %d Addr: %s Err: %d\n", n, addr, err)
+
+		p, err := tftp.ParsePacket(buf)
+
+		fmt.Printf("Parsed Packet Err: %d Packet: %+v \n", err, p)
 
 		go serve(pc, addr, buf[:n])
 	}
@@ -33,9 +45,19 @@ func main() {
 }
 
 func serve(pc net.PacketConn, addr net.Addr, buf []byte) {
-	// 0 - 1: ID
-	// 2: QR(1): Opcode(4)
-	//buf[2] |= 0x80 // Set QR bit
 
-	pc.WriteTo(buf, addr)
+	//var b bytes.Buffer
+	//b.WriteString("Server Reply")
+	//pc.WriteTo(b.Bytes(), addr)
+
+
+	// Construct an ack packet and send it to the client
+
+	var pr_send tftp.PacketAck
+	pr_send.BlockNum = 1
+
+	b := make([]byte, 1024)
+	b = pr_send.Serialize()
+
+	pc.WriteTo(b, addr)
 }
